ba4c7f5420b72e3e6e9e30c364104ebb362c21cd
nitis <nitish8152@gmail.com>
2023-01-06 23:48:12 +0530

    Added:bind the input element to the user name

--- src/App.js
+++ src/App.js
@@ -7,20 +7,28 @@ class App extends React.Component{
   url = "https://www.bridgelabz.com/"
   constructor(){
     super()
-    this.state={title:"Hello From Bridgelbaz"}
+    this.state={userName:' '}
   }
   //On Click Action
   onClick =($event) => {
     console.log("save button is clicked", $event);
-    window.open(this.url,"_blanck");
+    window.open(this.url,"_blank");
+  }
+
+  onNameChange = (event) => {
+    console.log("value is ", event.target.value);
+    this.setState({userName: event.target.value})
   }
 
 render(){
   return(
     <div className='App'>
-      <h2>{this.state.title}</h2>
+      <h1>Hello {this.state.userName} from Bridgelabz</h1>
       <img src={logo} onClick={this.onClick}
       alt ="The Bridgelabz logo: a Bridge to Employement through lab Works"/>
+      <div>
+        <input onChange={this.onNameChange}/>
+      </div>
     </div>
   );
 }
