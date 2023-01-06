1cdd7711b16fac7a911808387d338384b41af5e5
nitis <nitish8152@gmail.com>
2023-01-06 23:37:47 +0530

    Added:using property bindings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import logo from './logo.svg'
 import React from 'react';
 import './App.css';
+
 
 class App extends React.Component{
   constructor(){
@@ -11,9 +13,9 @@ render(){
   return(
     <div className='App'>
       <h2>{this.state.title}</h2>
+      <img src={logo}/>
     </div>
   );
 }
 }
-
 export default App;
