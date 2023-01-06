c564ca2444594a041cc6fd85f19d84c3456eb3ef
nitis <nitish8152@gmail.com>
2023-01-06 23:42:42 +0530

    Added:using event bindings technique

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import './App.css';
 
 
 class App extends React.Component{
+  url = "https://www.bridgelabz.com/"
   constructor(){
     super()
     this.state={title:"Hello From Bridgelbaz"}
+  }
+  //On Click Action
+  onClick =($event) => {
+    console.log("save button is clicked", $event);
+    window.open(this.url,"_blanck");
   }
 
 render(){
   return(
     <div className='App'>
       <h2>{this.state.title}</h2>
-      <img src={logo}/>
+      <img src={logo} onClick={this.onClick}
+      alt ="The Bridgelabz logo: a Bridge to Employement through lab Works"/>
     </div>
   );
 }
